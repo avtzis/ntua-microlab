b91aa23c404510bb885fbdf52645b9ffa9bdc8f9
ntua-el19230 <62750670+ntua-el19230@users.noreply.github.com>
2023-01-09 21:23:54 +0200

    Create betterversion.c

---
+++ betterversion.c
@@ -0,0 +1,7 @@
+https://www.youtube.com/watch?v=5qm8PH4xAss
+Go, go, go, go, go, go
+Go, shorty
+It's your birthday
+We gon' party like it's your birthday
+We gon' sip Bacardi like it's your birthday
+And you know we don't give a fuck it's not your birthday
